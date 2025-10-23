33080e82802c3cef4d45e826c2d44e976ab4db32
Kelvin Toyama <kelvintoyama28@gmail.com>
2025-10-22 21:26:43 -0300

    Atualzando o Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia n = new Nokia("123456789", "Nokia 6.1", "123456789012345", 32);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\nSmartphone iPhone:");
+Iphone i = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Telegram");
